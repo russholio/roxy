24dbf29955eb9fb76a320a23675acfeb41197fae
Russell Holman <rholman@ultraserve.com.au>
2012-11-01 15:04:42 +1100

    Initial commit of Roxy

--- README.md
+++ README.md
@@ -1,4 +1,97 @@
 Roxy
 ====
+Roxy is a simple proxy intended for use with javascript frameworks to enable RESTful inter domain requests.
+Why do we need a proxy?
+-----------------------
+JSONP requests that enable inter-domain AJAX requests do no allow for PUT,POST,DELETE,PATCH methods to be used.
+Additionally JSONP does not allow you to set additional headers in the request.
 
-A JSONP, RESTful proxy allowing for cross-domain requests
+The use of headers and these other HTTP request methods is essential to utilising the RESTful methodology to its full extent.
+
+Why use node.js?
+------------
+Since this Proxy is intended for use with client side javascript frameworks, we thought it would be useful to keep the proxy
+code in the same language to make it easy for the users to contribute/alter where they see fit.
+
+Requirements
+------------
+
+* node.js
+* qs (querystring npm library)
+
+Installing node.js on OS X:
+
+    brew update
+    brew install node
+
+
+To install the require npm package type:
+
+    npm install qs
+
+Configuring to work with NGINX (nginx.conf):
+
+    server {
+        listen 80;
+        server_name ms;
+        root /WEB_ROOT;
+        index index.html;
+        error_page 404 index.html;
+
+        location /roxy {
+            proxy_pass    http://127.0.0.1:3000/;
+        }
+    }
+
+Node.js script must also be run with:
+
+    node roxy.js
+
+Usage
+-----
+
+Send a JSONP AJAX request to roxy where the url is as so
+
+    http://localhost/roxy/remote/path
+
+Where 'localhost' is the server running the node.js file.
+And roxy is the location configured in the nginx.conf
+
+remote/path denotes the path you are trying to retrieve/modify from the RESTful API
+
+Along with this also pass any parameters (post data) you wish to send to the remote server.
+Also including the special _request parameter this will carry configuration data to Roxy.
+
+    _request = {
+        url: 'http://restful.api:8080',
+        method: 'POST',
+        headers: {
+            Authorization: 'OAuth 0123456789abcdef0123456789abcdef'
+        }
+    }
+
+This special parameter will connect Roxy to the specified server (restful.api)
+using the specified protocol (http) on port (8080).
+
+The method (POST) denotes the HTTP action you wish to perform and any headers set will also be forwarded.
+
+jQuery example:
+
+    jQuery.ajax({
+        url: http://localhost/roxy/remote/path,
+        data: {
+            _request = {
+                url: 'http://restful.api:8080',
+                method: 'POST',
+                headers: {
+                    Authorization: 'OAuth 0123456789abcdef0123456789abcdef'
+                }
+            },
+            text1: 'This is some data you want to send',
+            text2: 'This is some more data wish to send'            
+        },
+        dataType: 'jsonp',
+        success: function(data) {
+            alert(data);
+        }
+    });
---
+++ server.js
@@ -0,0 +1,110 @@
+var HOST = process.env.npm_package_config_hostname;
+var PORT = process.env.npm_package_config_port;
+
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var qs = require("qs");
+
+var restService = function(request) {
+    return new function() {
+        this.remoteUrl = url.parse(request.url);
+        this.protocol = 'https:';
+        this.port = 443;
+        if (this.remoteUrl.protocol == 'http:') {
+            this.protocol = this.remoteUrl.protocol;
+            this.port = 80;
+        }
+        
+        if (typeof this.remoteUrl.port != 'undefined') {
+            this.port = this.remoteUrl.port;
+        }
+        
+        this.options = {
+            'hostname' : this.remoteUrl.hostname,
+            'port' : this.port,
+            'encoding' : 'utf8',
+            'method' : request.method,
+            'path' : '/',
+            'headers' : { 'Accept' : 'application/json' }
+        };
+        
+        for (var property in request.headers) {
+            this.options.headers[property] = request.headers[property];
+        }
+        
+        this.request = function(path, success, error) {
+            var options = this.options;
+            options.path = this.remoteUrl.path+path;
+            
+            //console.log(options);
+            
+            var lhRequest; 
+            if (this.protocol === 'http:') {
+                lhRequest = http.request(options, success);
+            }
+            else {
+                lhRequest = https.request(options, success);
+            }
+            
+            lhRequest.on('error', error);
+            
+            return lhRequest;        
+        }
+    }
+}
+
+var server = http.createServer(function(req, res) {
+    var roxyResponse = res;
+    var params = qs.parse(url.parse(req.url, true).search.substr(1));
+    //console.log(params);
+    
+    var request = params._request;    
+    request.method = typeof request.method == 'undefined' ? 'GET' : request.method.toUpperCase();
+    var path = url.parse(req.url).pathname.substr(2);
+    var callback = params.callback;
+    
+    delete params._;
+    delete params.callback;
+    delete params._request;
+    
+    var remoteRequest = restService(request).request(
+        path, 
+        function (lhResponse) {
+            lhResponse.responseString = '';
+            //console.log('STATUS: ' + lhResponse.statusCode);
+            lhResponse.setEncoding('utf8');
+            lhResponse.on('data', function (chunk) {
+                this.responseString += chunk;
+            });
+            lhResponse.on('end', function () {
+                roxyResponse.setHeader('Content-Type', 'text/javascript');
+                roxyResponse.write(callback+'('+this.responseString+');');
+                roxyResponse.end();
+                //console.log(roxyResponse);
+                //console.log('END: '+this.responseString);
+            });
+            lhResponse.on('close', function () {
+                roxyResponse.close();
+                console.log('CLOSE');
+            });
+        },
+        function (lhError) {
+            console.log('problem with request: ' + lhError.message);
+            roxyResponse.writeHead(500);
+            roxyResponse.end(lhError.message);
+        }
+    );
+    
+    var lhParamString = ''
+    if (request.method != 'GET') {
+        //console.log(request.method);
+        lhParamString = qs.stringify(params);
+        //console.log(lhParamString);
+        remoteRequest.setHeader('Content-Length', lhParamString.length);
+    }
+    remoteRequest.write(lhParamString);
+    remoteRequest.end();
+});
+
+server.listen(PORT, HOST);
